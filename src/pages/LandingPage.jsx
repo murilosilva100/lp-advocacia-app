a54ab0f2bd4b715b9c690128a0cdebafe0db7e5f
Murilo Silva <murilofariassilva50@gmail.com>
2025-10-17 12:14:17 -0300

    primeiro commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header";
+import Hero from "../components/hero";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
